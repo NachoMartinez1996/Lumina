7157fb990bcf271dfd4a91611b2143500bbd2202
Ignacio Martínez <91094426+NachoMartinez1996@users.noreply.github.com>
2026-04-06 16:58:02 -0300

    Versión 4.25

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION_CACHE = '4.24';
+const VERSION_CACHE = '4.25';
 const CACHE_SHELL = `lumina-shell-${VERSION_CACHE}`;
 const CACHE_RUNTIME = `lumina-runtime-${VERSION_CACHE}`;
 const APP_SHELL = './index.html';
