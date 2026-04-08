c3196bc13173feb653e80a04b7d4bb23d7e93e93
Ignacio Martínez <91094426+NachoMartinez1996@users.noreply.github.com>
2026-04-08 02:04:28 -0300

    4.294.846
    Comentario Versículo

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION_CACHE = '4.294.845';
+const VERSION_CACHE = '4.294.846';
 const CACHE_SHELL = `lumina-shell-${VERSION_CACHE}`;
 const CACHE_RUNTIME = `lumina-runtime-${VERSION_CACHE}`;
 const APP_SHELL = './index.html';
