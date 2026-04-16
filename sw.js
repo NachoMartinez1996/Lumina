af4a6ad7b8fe00019c1f1fb10172e6658c33c83c
Ignacio Martínez <91094426+NachoMartinez1996@users.noreply.github.com>
2026-04-16 11:50:15 -0300

    Prueba orden AT

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION_CACHE = '4.295.021';
+const VERSION_CACHE = '4.295.022';
 const CACHE_SHELL = `lumina-shell-${VERSION_CACHE}`;
 const CACHE_RUNTIME = `lumina-runtime-${VERSION_CACHE}`;
 const APP_SHELL = './index.html';
